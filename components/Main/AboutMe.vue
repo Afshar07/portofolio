0761a1e9068e57d860c858646ac46d75a541a7f5
Afshar07 <mr.afshar0@gmail.com>
2022-11-25 19:51:03 +0330

    Handle about me section

--- components/Main/AboutMe.vue
+++ components/Main/AboutMe.vue
@@ -1,50 +1,23 @@
 <template>
-  <div class="w-full flex items-center text-white">
-    <div class="w-1/2">
-      <h2 class="text-xl">WEB DEVELOPER</h2>
-      <h1
-        class="text-6xl my-3 text-transparent bg-clip-text bg-gradient-to-r from-gradient-first to-gradient-second"
-      >
-        Amir Afshar
-      </h1>
-      <p class="text-secondary mt-10 mb-6">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-        deserunt eligendi vitae eaque libero non, distinctio fugiat. Ut dicta
-        saepe reprehenderit veniam? Provident iusto labore voluptate sint iure
-        impedit qui quisquam sunt sit nostrum? Vero eaque impedit praesentium
-        cumque cupiditate commodi, eum ipsum dolore ut dolorem accusantium porro
-        beatae aperiam.
+  <div class="w-full mt-6 mb-20">
+    <h3 class="text-xl mb-1 text-secondary ml-4">About me</h3>
+    <div class="flex items-center relative">
+      <div
+        class="absolute rounded-md mt-2 left-0 bg-gradient-to-b from-gradient-first to-gradient-second w-[5px] h-full"
+      ></div>
+      <p class="text-secondary ml-4">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+        distinctio provident animi recusandae nam, eius totam dolore earum
+        dolores officiis impedit. Magnam repellat quia, ad odit quis unde
+        incidunt nisi quas, exercitationem qui amet labore nesciunt optio
+        sapiente delectus quibusdam! At itaque harum dolor dolore nesciunt fugit
+        magni consequuntur suscipit?
       </p>
-      <button
-        type="button"
-        class="mt-3 px-5 py-3 bg-gradient-to-r from-gradient-first to-gradient-second rounded-md text-primary"
-      >
-        Contact Me
-      </button>
     </div>
-    <div class="w-1/2">
-      <div class="images w-[500px] h-[500px] flex items-center justify-center">
-        <MainMyImage></MainMyImage>
-      </div>
-    </div>
-  </div>
-  <div class="w-full flex items-center justify-center">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="w-6 h-6 text-white cursor-pointer"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
-      />
-    </svg>
   </div>
 </template>
-
-<script setup></script>
-<style scoped></style>
+<style scoped>
+.horizon-divider ::before {
+  content: "";
+}
+</style>

096455c2a5a89e194522717b3698fa278f920aa7
Afshar07 <mr.afshar0@gmail.com>
2022-11-25 19:29:37 +0330

    Handle first section of homepage

--- components/Main/AboutMe.vue
+++ components/Main/AboutMe.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="w-full flex items-center text-white">
-    <div class="w-1/2">test</div>
+    <div class="w-1/2">
+      <h2 class="text-xl">WEB DEVELOPER</h2>
+      <h1
+        class="text-6xl my-3 text-transparent bg-clip-text bg-gradient-to-r from-gradient-first to-gradient-second"
+      >
+        Amir Afshar
+      </h1>
+      <p class="text-secondary mt-10 mb-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+        deserunt eligendi vitae eaque libero non, distinctio fugiat. Ut dicta
+        saepe reprehenderit veniam? Provident iusto labore voluptate sint iure
+        impedit qui quisquam sunt sit nostrum? Vero eaque impedit praesentium
+        cumque cupiditate commodi, eum ipsum dolore ut dolorem accusantium porro
+        beatae aperiam.
+      </p>
+      <button
+        type="button"
+        class="mt-3 px-5 py-3 bg-gradient-to-r from-gradient-first to-gradient-second rounded-md text-primary"
+      >
+        Contact Me
+      </button>
+    </div>
     <div class="w-1/2">
       <div class="images w-[500px] h-[500px] flex items-center justify-center">
-        <div class="relative">
-          <img src="/bg-bubble.png" class="w-full" alt="" />
-          <img src="/codes.png" class="absolute top-7 w-5/6 left-6" alt="" />
-          <img
-            src="plant.png"
-            class="absolute right-0 w-1/5 bottom-[6px]"
-            alt=""
-          />
-          <img src="/desk.png" alt="" />
-          <img src="/chair.png" class="absolute bottom-[6px] left-8" alt="" />
-          <img src="/me.png" class="absolute bottom-[6px] left-16" alt="" />
-          <img
-            src="/laptop.png"
-            class="absolute bottom-[6px] right-14"
-            alt=""
-          />
-        </div>
+        <MainMyImage></MainMyImage>
       </div>
     </div>
   </div>
---
+++ components/Main/MyImage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative">
+    <img src="/bg-bubble.png" class="w-full" alt="" />
+    <img src="/codes.png" class="absolute top-7 w-5/6 left-6" alt="" />
+    <img src="plant.png" class="absolute right-0 w-1/5 bottom-[6px]" alt="" />
+    <img src="/desk.png" alt="" />
+    <img src="/chair.png" class="absolute bottom-[6px] left-8" alt="" />
+    <img src="/me.png" class="absolute bottom-[6px] left-16" alt="" />
+    <img src="/laptop.png" class="absolute bottom-[6px] right-14" alt="" />
+  </div>
+</template>

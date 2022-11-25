14d092c8303cdf9cff84d58878e10a9614ae1723
Afshar07 <mr.afshar0@gmail.com>
2022-11-25 19:32:34 +0330

    Add arrow icon

--- components/Main/AboutMe.vue
+++ components/Main/AboutMe.vue
@@ -28,6 +28,22 @@
       </div>
     </div>
   </div>
+  <div class="w-full flex items-center justify-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6 text-white cursor-pointer"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
+      />
+    </svg>
+  </div>
 </template>
 
 <script setup></script>
